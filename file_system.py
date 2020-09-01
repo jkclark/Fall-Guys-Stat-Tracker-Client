5ff552f904caa6edcfbbeed090c762ae026caf49
Josh Clark <jclarktennis@gmail.com>
2020-08-31 22:19:18 -0400

    Successfully retrieving account name.

--- file_system.py
+++ file_system.py
@@ -1,32 +1,31 @@
-#  import os
+import os
 import sys
+import vdf
 import winreg
 
 
 def get_num_system_bits():
     if sys.maxsize == 2_147_483_647:
         return 32
-    elif sys.maxsize == 9_223_372_036_854_775_807:
-        return 64
+    return 64
+
+
+def get_steam_installation_location():
+    if get_num_system_bits() == 32:
+        KEY_NAME = r'SOFTWARE\Valve\Steam'
     else:
-        assert False, 'System is neither 32bit nor 64-bit.'
+        KEY_NAME = r'SOFTWARE\WOW6432Node\Valve\Steam'
+
+    registry_key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, KEY_NAME)
+    value, _ = winreg.QueryValueEx(registry_key, 'InstallPath')
+    winreg.CloseKey(registry_key)
+    return value
 
 
-def get_steam_installation_folder():
-    NUM_SYSTEM_BITS = get_num_system_bits()
-    if NUM_SYSTEM_BITS == 32:
-        KEY_NAME = r'HKEY_LOCAL_MACHINE\SOFTWARE\Valve\Steam'
-    else:
-        KEY_NAME = r'HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Valve\Steam'
+def get_steam_account_name():
+    with open(os.path.join(get_steam_installation_location(), r'config\loginusers.vdf'), 'r') as logins_file:
+        logins = vdf.loads(logins_file.read())
 
-    try:
-        registry_key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, KEY_NAME, 0, winreg.KEY_READ)
-        value, regtype = winreg.QueryValueEx(registry_key, 'InstallPath')
-        winreg.closeKey(registry_key)
-        return value
-    except WindowsError:
-        return ''
-
-
-def get_steam_username():
-    print('Steam installation:', get_steam_installation_folder())
+    # Return the first user's account name
+    for user in logins['users']:
+        return logins['users'][user]['AccountName']
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from file_system import get_steam_username
+from file_system import get_steam_account_name
 
 
 def main():
-    get_steam_username()
+    print(get_steam_account_name())
 
 
 if __name__ == "__main__":
