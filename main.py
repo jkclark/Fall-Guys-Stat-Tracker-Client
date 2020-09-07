0cf50f023f42576a5acad5f2000ca1f1a59c532b
Josh Clark <jclarktennis@gmail.com>
2020-09-06 21:03:18 -0400

    Now sending info to backend. Also started with compiling to exe.

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-from file_system import get_steam_account_name
+from json import dumps
+from requests import post
+
+from parse import LogParser
+from file_system import follow_file, get_fall_guys_log_location, get_steam_id
 
 
 def main():
-    print(get_steam_account_name())
+    STEAM_ID = get_steam_id()
+
+    with open(get_fall_guys_log_location(), 'r') as log_file:
+        for episode in LogParser(follow_file(log_file)).parse():
+            post(
+                'http://localhost:5000/client/',
+                data=dumps({'steam_id': STEAM_ID, 'episode_info': episode})
+            )
 
 
 if __name__ == "__main__":
